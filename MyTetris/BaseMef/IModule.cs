762f5bb839fd9a58d4cf5ab8312f8cda3559312d
elpablos@gmail.com <elpablos@gmail.com@3f24f4ca-0bdf-9821-8f88-2a6b6d8ab6c0>
2012-07-16 12:58:53 +0000

    + MEF test

---
+++ MyTetris/BaseMef/IModule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.Composition;
+
+namespace BaseMef
+{
+    [InheritedExport]
+    public interface IModule
+    {
+        string DisplayName { get; }
+    }
+}
---
+++ MyTetris/MefLibB/B.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BaseMef;
+
+namespace MefLibB
+{
+    public class B : IModule
+    {
+        public string DisplayName
+        {
+            get { return "B"; }
+        }
+    }
+}
